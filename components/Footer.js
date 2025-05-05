dcb3debc9390c6e76feba7c718151d8cb9ff6170
Levi Hackermann <76484713+LeviHackermann@users.noreply.github.com>
2025-05-05 19:32:06 +0200

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 py-8 mt-16">
-      <div className="container mx-auto px-6 text-center">
-        <p>© {new Date().getFullYear()} MMT Projects. Alle rechten voorbehouden.</p>
-      </div>
-    </footer>
-  );
-}
