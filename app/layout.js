9aef75b9eaff0e85a5fa7c6930396bb8ab7084e0
Levi Hackermann <76484713+LeviHackermann@users.noreply.github.com>
2025-05-05 18:20:50 +0200

    Delete app directory

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="nl">
-      <body style={{ fontFamily: 'sans-serif', margin: 0, padding: 0 }}>
-        {children}
-      </body>
-    </html>
-  );
-}
